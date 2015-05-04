2328812ad5385706c8ff6a0ff1a5fc5ee049f80a
David <xudavidb@gmail.com>
2015-05-03 23:20:58 -0700

    add serve task to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,8 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-broccoli')
 
+  grunt.registerTask('default', 'watch')
   grunt.registerTask('watch', ['broccoli:dist:watch'])
   grunt.registerTask('build', ['broccoli:dist:build'])
+  grunt.registerTask('serve', ['broccoli:dist:serve'])
 }
