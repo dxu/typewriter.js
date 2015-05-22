e7f29e6f81179573d0e2fc7ccfa3aad302654ee0
David <xudavidb@gmail.com>
2015-05-21 22:39:53 -0700

    fix typing method and fix promise resolving

--- client/scripts/scripts.js
+++ client/scripts/scripts.js
@@ -1,10 +1,8 @@
-console.log('he', Typewriter)
-
 document.addEventListener("DOMContentLoaded", function(event) {
   var t = new Typewriter({
     lps: 20,
     container: document.getElementById('test')
   });
 
-  t.type('hjwefaweofauwhrgiuaehrgiuahweoauwhefoawuhgeoaerughaoriguhaowiughaowriguhaowriguhaorguh')
+  t.type('hjwefaweofauwhrgiuaehrgiuahweoauwhefoawuhgeoaerughaoriguhaowiughaowriguhaowriguhaorguh').beginTag('p')
 })
--- src/typewriter.js
+++ src/typewriter.js
@@ -8,6 +8,8 @@ window.Typewriter = function(options) {
   this.container = options.container
   // start off with the current element being a div
   this.currentElement = this.container
+  // used for maintaining order of execution, immediately resolve the first op
+  this.currentPromise = Promise.resolve()
   return this
 }
 
@@ -17,26 +19,24 @@ Typewriter.prototype.feed = function(container) {
   return this
 }
 
-// insert a carriage return in the form of a br tag
-Typewriter.prototype.cr = function() {
-  this.currentElement.appendChild(document.createElement("br"));
-  return this
-}
-
 // creates and puts a text node
 Typewriter.prototype.type = function(text) {
-  var letterCount
-    , interval;
-
-  letterCount = 0;
-  interval = setInterval(() => {
-    this.currentElement.innerHTML += text[letterCount]
-    letterCount++
-    if(letterCount === text.length) {
-      clearInterval(interval);
-    }
-  }, 1/this.lps * 1000);
-
+  this.currentPromise = this.currentPromise.then(() => {
+    var letterCount
+      , interval;
+    letterCount = 0;
+    return new Promise((resolve) => {
+      interval = setInterval(() => {
+        this.currentElement.innerHTML += text[letterCount]
+        letterCount++
+        // finished typing
+        if(letterCount === text.length) {
+          clearInterval(interval);
+          resolve()
+        }
+      }, 1/this.lps * 1000)
+    })
+  }).catch(console.log.bind(console))
   return this
 }
 
@@ -46,18 +46,46 @@ Typewriter.prototype.type = function(text) {
 
 // create a tag, and set that as the current operating element
 Typewriter.prototype.beginTag = function(tag, classes, id) {
-  this.currentElement = document.createElement(tag)
-  this.container.currentElement.appendChild(this.currentElement)
+  this.currentPromise = this.currentPromise.then(() => {
+    var newTag = document.createElement(tag)
+    this.currentElement.appendChild(newTag)
+    this.currentElement = newTag
+    return Promise.resolve()
+  }).catch(console.log.bind(console))
   return this
 }
 
-// pause the typewriter
+// insert a carriage return in the form of a br tag
+Typewriter.prototype.cr = function() {
+  this.currentElement.appendChild(document.createElement("br"));
+  return this
+}
+
+// change typing speed
+Typewriter.prototype.changeLPS = function(lps) {
+  this.currentPromise = this.currentPromise.then(() => {
+    this.lps = lps
+    return Promise.resolve()
+  })
+  return this
+}
+
+// disable cursor tick
+Typewriter.prototype.stopTick = function() {
+  return this
+}
+
+// enable cursor tick
+Typewriter.prototype.startTick = function() {
+  return this
+}
+
+// pause the typewriter, in milliseconds
 Typewriter.prototype.pause = function(duration) {
-  setTimeout(function(){}, duration)
   return this
 }
 
 // finish this container
 Typewriter.prototype.endFeed = function(container) {
-
+  return this
 }
